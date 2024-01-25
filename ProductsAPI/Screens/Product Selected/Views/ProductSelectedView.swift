f91ce3b0c947f06289cf744abe9299c90748e41d
Vincent Cubit <vinniecubit@yahoo.com>
2024-01-25 13:48:13 -0600

    Progress on selected a product view

--- ProductsAPI/Screens/Product Selected/Views/ProductSelectedView.swift
+++ ProductsAPI/Screens/Product Selected/Views/ProductSelectedView.swift
@@ -10,17 +10,42 @@ import SwiftUI
 struct ProductSelectedView: View {
   
   let productSelectedViewModel: ProductSelectedViewModel
+    
+  @State private var selectedImageIndex: Int = 0
+  @State private var topImageUrlString: String? = nil
   
   var body: some View {
     NavigationStack {
-      VStack {
-        AsyncImage(url: URL(string: productSelectedViewModel.product.thumbnail)!)
+      VStack(spacing: 0.0) {
+        AsyncImage(url: URL(string: topImageUrlString ?? productSelectedViewModel.product.thumbnail)!)
           .aspectRatio(contentMode: .fit)
-        LazyHStack {
-          ForEach(productSelectedViewModel.product.images, id: \.self) { image in
-            AsyncImage(url: URL(string: image)!)
-              .frame(width: 20.0, height: 20.0)
+          .onAppear {
+            topImageUrlString = productSelectedViewModel.product.thumbnail
           }
+        HStack {
+          ScrollView {
+            LazyHStack {
+              ForEach(0..<productSelectedViewModel.product.images.count, id: \.self) { index in
+                AsyncImage(url: URL(string: productSelectedViewModel.product.images[index])!,
+                           content: { image in
+                              image.resizable()
+                              .border(selectedImageIndex == index ? Color(UIColor.label) : Color.clear, width: 4)
+                                .onTapGesture {
+                                  selectedImageIndex = index
+                                  topImageUrlString = productSelectedViewModel.product.images[index]
+                                }
+                              .aspectRatio(contentMode: .fit)
+                              .cornerRadius(8.0)
+                              .frame(maxWidth: 100.0, maxHeight: 100.0)
+                           },
+                           placeholder: {
+                            ProgressView()
+                           })
+              }
+            }
+          }
+          .padding(.leading)
+          Spacer()
         }
         Spacer()
       }
@@ -32,6 +57,6 @@ struct ProductSelectedView: View {
     ProductSelectedView(productSelectedViewModel: ProductSelectedViewModel(product: Product(id: 0, title: "Test", description: "Test", price: 70, discountPercentage: 80, rating: 5, stock: 55, brand: "Test", category: "Test", thumbnail: "https://cdn.dummyjson.com/product-images/8/1.jpg", images: [
       "https://cdn.dummyjson.com/product-images/8/1.jpg",
       "https://cdn.dummyjson.com/product-images/8/2.jpg",
-      "https://cdn.dummyjson.com/product-images/8/3.jpg"
+//      "https://cdn.dummyjson.com/product-images/8/3.jpg"
   ])))
 }
