80712d61a7c53ce511f3b279fab6b56e256aa6f7
Vincent Cubit <vinniecubit@yahoo.com>
2024-04-26 17:31:30 -0500

    added unit tests for APIService

--- ProductsAPITests/Networking/APIService/APIServiceTests.swift
+++ ProductsAPITests/Networking/APIService/APIServiceTests.swift
@@ -58,4 +58,35 @@ final class APIServiceTests: XCTestCase {
     
     XCTAssertEqual(result, staticJSON, "The returned response should be decoded correctly.")
   }
+  
+  func test_invalid_json_response_is_invalid() async throws {
+    guard let url = Bundle(for: type(of: self)).url(forResource: "MockProducts", withExtension: "json") else {
+      XCTFail("Failed to find the static products file url.")
+      return
+    }
+    
+    guard let data = try? Data(contentsOf: url) else {
+      XCTFail("Failed to get data from the static products file.")
+      return
+    }
+    
+    MockURLSessionProtocol.loadingHandler = {
+      let response = HTTPURLResponse(url: self.url,
+                                     statusCode: 200,
+                                     httpVersion: nil,
+                                     headerFields: nil)
+      
+      return (data, response!)
+    }
+    
+    do {
+      _ = try await APIService.request(session: session,
+                                                     [Product].self,
+                                                     from: ProductsAPI.getProducts(skip: 0, limit: 20))
+    } catch {
+      if error is APIError {
+        XCTFail("The error should be a system decoding error.")
+      }
+    }
+  }
 }
